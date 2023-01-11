aa0755474a87c0379b08958a425982b288b2c690
FrederikSchibelfeldt <61239673+FrederikSchibelfeldt@users.noreply.github.com>
2023-01-11 21:41:32 +0100

    Added things for repository

---
+++ ReportService/src/main/java/ReportService/Payment.java
@@ -0,0 +1,4 @@
+package ReportService;
+
+public class Payment {
+}
--- ReportService/src/main/java/Repository/PaymentRepository.java
+++ ReportService/src/main/java/Repository/PaymentRepository.java
@@ -1,4 +1,29 @@
 package Repository;
 
-public class PaymentRepository {
+import ReportService.Payment;
+
+import java.util.List;
+
+public class PaymentRepository implements IPaymentRepository {
+
+
+    @Override
+    public void addPayment() {
+        
+    }
+
+    @Override
+    public List<Payment> GetAllPayments() {
+        return null;
+    }
+
+    @Override
+    public List<Payment> GetCustomerPayments(String cid) {
+        return null;
+    }
+
+    @Override
+    public List<Payment> GetMerchantPayments(String mid) {
+        return null;
+    }
 }
