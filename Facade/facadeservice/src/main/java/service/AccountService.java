3331803066935752f546436aa0dfeee65b47e80a
joaomena <joao.17.2001@gmail.com>
2023-01-17 02:05:49 +0100

    account service event handling in the facade

--- Facade/facadeservice/src/main/java/service/AccountService.java
+++ Facade/facadeservice/src/main/java/service/AccountService.java
@@ -1,7 +1,31 @@
 package service;
 
+import entities.DTUPayUSer;
+import EventTypes;
+
+import java.util.concurrent.CompletableFuture;
+
 import messaging.Event;
 import messaging.MessageQueue;
-
 public class AccountService {
+
+    private MessageQueue queue;
+    private CompletableFuture<DTUPayUser> user;
+
+    public AccountService(MessageQueue q){
+        queue = q;
+        queue.addHandler(REGISTER_ACCOUNT_COMPLETED, this::handleAccountRegistration);
+    }
+
+    public DTUPayUser registerAccount(DTUPayUser account){
+        user = new CompletableFuture<>();
+        Event event = new Event(REGISTER_ACCOUNT_REQUEST, new Object[] { account });
+        queue.publish(event);
+        return user.join();
+    }
+
+    public void handleAccountRegistration(Event e){
+        var acc = e.getArgument(0, DTUPayUser.class);
+        user.complete(acc);
+    }
 }
