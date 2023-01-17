861f1eeec71f1c292fb01d7839f91e5dbd38fad0
joaomena <joao.17.2001@gmail.com>
2023-01-17 18:27:06 +0100

    add rest interface for customers and merchants in facade

--- Facade/facadeservice/src/main/java/resource/CustomerResource.java
+++ Facade/facadeservice/src/main/java/resource/CustomerResource.java
@@ -1,4 +1,29 @@
 package resource;
 
+import entities.DTUPayUser;
+import service.AccountService;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+import javax.ws.rs.POST;
+import javax.ws.rs.core.MediaType;
+
+@Path("/customers")
 public class CustomerResource {
+
+    private AccountService service = new DTUPayFactory().getService().getAccountService();
+
+    @POST
+    @Path("/register")
+    @Produces(MediaType.APPLICATION_JSON)
+    public DTUPayUser registerCustomer(DTUPayUser customer){
+        return service.requestAccountRegister(customer);
+    }
+
+    @POST
+    @Path("/unregister")
+    @Produces(MediaType.APPLICATION_JSON)
+    public DTUPayUser unregisterCustomer(DTUPayUser customer){
+        return service.requestAccountDelete(customer);
+    }
 }
--- Facade/facadeservice/src/main/java/resource/MerchantResource.java
+++ Facade/facadeservice/src/main/java/resource/MerchantResource.java
@@ -1,4 +1,29 @@
 package resource;
 
+import entities.DTUPayUser;
+import service.AccountService;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+import javax.ws.rs.POST;
+import javax.ws.rs.core.MediaType;
+
+@Path("/merchants")
 public class MerchantResource {
+
+    private AccountService service = new DTUPayFactory().getService().getAccountService();
+
+    @POST
+    @Path("/register")
+    @Produces(MediaType.APPLICATION_JSON)
+    public DTUPayUser registerMerchant(DTUPayUser merchant){
+        return service.requestAccountRegister(merchant);
+    }
+
+    @POST
+    @Path("/unregister")
+    @Produces(MediaType.APPLICATION_JSON)
+    public DTUPayUser unregisterMerchant(DTUPayUser merchant){
+        return service.requestAccountDelete(merchant);
+    }
 }
