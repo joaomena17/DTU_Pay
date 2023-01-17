a5ef0d5080417c8b64f280cdc21bbe4e938566d0
joaomena <joao.17.2001@gmail.com>
2023-01-17 11:17:36 +0100

    added dtupay service and factory

--- Facade/facadeservice/src/main/java/service/DTUPayService.java
+++ Facade/facadeservice/src/main/java/service/DTUPayService.java
@@ -1,4 +1,31 @@
 package service;
 
-public class DTUPayService {
+import messaging.MessageQueue;
+
+public class DTUPayService<T> {
+    private final AccountService<T> account;
+    private final PaymentService<T> payment;
+    private final ReportService<T> report;
+    private final TokenService<T> token;
+
+    public DTUPayService(MessageQueue q){
+        this.account = AccountService<T>(q);
+        this.payment = PaymentService<T>(q);
+        this.report = ReportService<T>(q);
+        this.token = TokenService<T>(q);
+    }
+
+    public AccountService<T> getAccountService(){ return account; }
+
+    public PaymentService<T> getPaymentService() {
+        return payment;
+    }
+
+    public ReportService<T> getReportService() {
+        return report;
+    }
+
+    public TokenService<T> getTokenService() {
+        return token;
+    }
 }
