7dca39fe1f6f9c8cfc1469347b1544aea9806d38
Uhphonsoo <joao.afonso.hs@gmail.com>
2023-01-17 15:21:15 +0100

    Multiple changes
    Further complete account management
    Unify customer and merchant tests
    Refactor code

---
+++ AccountManagement/src/test/java/accountManagementService/UnregisterCustomerEndToEndSteps.java
@@ -0,0 +1,24 @@
+package accountManagementService;
+
+import io.cucumber.java.Before;
+
+public class UnregisterCustomerEndToEndSteps {
+
+    // private User user = new User();
+    // private String name = "Joao Afonso";
+    // private String bankId;
+    // private String role = "merchant";
+    // private BankService bank = new BankServiceService().getBankServicePort();
+    // private DTUPayUser merchant;
+    // private DTUPayUser expected;
+    // private CorrelationId correlationId;
+    // private AccountManagementService merchantService = new AccountManagementFactory().getService();
+    // private MessageQueue queue = mock(MessageQueue.class);
+    
+    /* Scenario:
+    Given a customer that is registered with DTU Pay
+    When the customer is being unregistered
+    Then the customer is unregistered */
+    
+
+}
