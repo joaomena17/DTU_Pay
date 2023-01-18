b27b4780b4361ba1e65f04da311f636d6916f125
joaomena <joao.17.2001@gmail.com>
2023-01-18 18:13:26 +0100

    update facade to main

---
+++ Facade/facadeservice/src/main/java/service/DTUPayService.java
@@ -0,0 +1,37 @@
+package service;
+
+import service.AccountService;
+import service.PaymentService;
+import service.TokenService;
+import service.ReportService;
+import messaging.MessageQueue;
+import java.util.concurrent.CompletableFuture;
+
+public class DTUPayService {
+
+    private final AccountService account;
+    private final PaymentService payment;
+    private final ReportService report;
+    //private final TokenService token;
+
+    public DTUPayService(MessageQueue q){
+        this.account = new AccountService(q);
+        this.payment = new PaymentService(q);
+        this.report = new ReportService(q);
+        //this.token = new TokenService(q);
+    }
+
+    public AccountService getAccountService(){ return account; }
+
+
+    public PaymentService getPaymentService() { return payment; }
+
+
+    public ReportService getReportService() {
+        return report;
+    }
+  /*
+    public TokenService<T> getTokenService() {
+        return token;
+    } */
+}
