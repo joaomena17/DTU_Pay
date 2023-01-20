ba3efd5db219acf48f0df366d4fab0de8233b1f5
tiagosilveriom <tiago.ars.machado@gmail.com>
2023-01-20 02:38:50 +0100

    cucumber tests file

---
+++ demo_client/src/test/java/CucumberTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin="summary"
+        , publish= false
+        , tags = "not @DontRun"
+        , features = "features"  // directory of the feature files
+)
+public class CucumberTest {
+}
